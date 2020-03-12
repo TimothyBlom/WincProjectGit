c90644228c1dadb82458d3a18f686ff37ccef9dc
kracok <kracok@gmail.com>
2020-03-12 17:40:40 +0100

    changes to the graph in home.js

--- projectStudenDashboard/src/Components/Home.js
+++ projectStudenDashboard/src/Components/Home.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import { VictoryBar, VictoryChart, VictoryAxis, VictoryStack } from 'victory';
-import studentdata from './StudentData';
+import { VictoryBar, VictoryChart, VictoryAxis, VictoryStack, VictoryGroup } from 'victory';
+import StudentData from './StudentData';
 
-const StudentData = [
-  {quarter: 1, earnings: 1, over: 3},
+const data = [
+  {quarter: 1, earnings: 4, over: 3},
   {quarter: 2, earnings: 3, over: 2},
   {quarter: 3, earnings: 4, over: 1},
   {quarter: 4, earnings: 5, over: 3},
@@ -13,25 +13,31 @@ const StudentData = [
 const Home = () => (
   <div>
     <h1>Home</h1>
-    <h3>hier moet een staafdiagram komen, die voor elke opdracht de gemiddelde score weergeeft van alle studenten voor hoe leuk en hoe moeilijk het was</h3>
-    <VictoryChart domainPadding={20}> 
+    <h2>Studenten score</h2>
+    {/* <StudentData /> */}
+    <VictoryChart>
     <VictoryAxis 
-      tickValues={["Quarter 1", "Quarter 2", "Quarter 3", "Quarter 4", "Quarter 5"]}/>
+      style={{ tickLabels: { angle: 45 } }}
+      tickValues={["SCRUM", "W1D1-1", "W1D2-1", "W1D2-2", "W1D2-3", 'W1D2-4', 'W1D2-5', 'W1D3-1', 'W1D3-2', 'W1D3-4', 'W1D3-5', 'W1D3 - Project - Guess-the-number', 'W1D4-1', 'W1D4 - Project - Kleurentoggle', 'W1D5 - Project - Galgje', 'W2D1-1', 'W2D1-2', 'W2D2-1', 'W2D2-2', 'W2D2-3', 'W2D3-1', 'W2D3-2', 'W2D3-3', 'W2D4-1', 'W2D4-2', 'W2D4-3', 'W2D5 - Project - Filmzoeker', 'W3D1-1', 'W3D1-2', 'W3D1-3', 'W3D1-4', 'W3D2-1', 'W3D2-2', 'W3D2-3', 'W3D3-1', 'W3D3-2', 'W3D3-3', 'W3D3-4', 'W3D4-1', 'W3D4-2', 'W3D5 - Project - Todo-List', 'W4D2-1', 'W4D2-2', 'W4D2-3', 'W4D2-4', 'W4D3-1', 'W4D3-2', 'W4D3-3', 'W4D3-4', 'W4D3-5', 'W4D3 - Project - Next-Level CSS', 'W5D4-1', 'W5D5 - Project - Lil_Playlist', 'W6D1-1', 'W6D2-1', 'W6D2 - Project - Eindopdracht']}
+    />
     <VictoryAxis 
-      dependentAxis />
-      <VictoryStack>
+      dependentAxis
+      tickFormat={['1', '2', '3', '4', '5']}
+    />
+    <VictoryGroup offset={20} colorScale={"qualitative"}>
     <VictoryBar
-      data={StudentData}
+      data={data}
       x={"quarter"}
       y={"earnings"}
     />
     <VictoryBar
-      data={StudentData}
+      data={data}
       x={"quarter"}
       y={"over"}
     />
-    </VictoryStack>
+    </VictoryGroup>
     </VictoryChart>
+
   </div>
 );
 
--- projectStudenDashboard/src/Components/StudentData.js
+++ projectStudenDashboard/src/Components/StudentData.js
@@ -1,11 +1,348 @@
 import React from 'react';
 
 const StudentData = [
-  {quarter: 1, earnings: 13000},
-  {quarter: 2, earnings: 16500},
-  {quarter: 3, earnings: 14250},
-  {quarter: 4, earnings: 19000},
-  {quarter: 5, earnings: 11000}
+  {
+    'Name': 'Evelyn',
+    'asignment': 'SCRUM',
+    'funRated': '4',
+    'dificulty': '3'
+  },
+  {
+    'Name': 'Evelyn',
+    'asignment': 'W1D1-1',
+    'funRated': '3',
+    'dificulty': '3'
+  },
+  {
+    'Name': 'Evelyn',
+    'asignment': 'W1D2-1',
+    'funRated': '3',
+    'dificulty': '1'
+  },
+  {
+    'Name': 'Evelyn',
+    'asignment': 'W1D2-2',
+    'funRated': '4',
+    'dificulty': '1'
+  },
+  {
+    'Name': 'Evelyn',
+    'asignment': 'W1D2-3',
+    'funRated': '3',
+    'dificulty': '2'
+  },
+  {
+    'Name': 'Evelyn',
+    'asignment': 'W1D2-4',
+    'funRated': '4',
+    'dificulty': '3'
+  },
+  {
+    'Name': 'Evelyn',
+    'asignment': 'W1D2-5',
+    'funRated': '4',
+    'dificulty': '3'
+  },
+  {
+    'Name': 'Evelyn',
+    'asignment': 'W1D3-1',
+    'funRated': '4',
+    'dificulty': '3'
+  },
+  {
+    'Name': 'Evelyn',
+    'asignment': 'W1D3-2',
+    'funRated': '4',
+    'dificulty': '3'
+  },
+  {
+    'Name': 'Evelyn',
+    'asignment': 'W1D3-4',
+    'funRated': '4',
+    'dificulty': '4'
+  },
+  {
+    'Name': 'Evelyn',
+    'asignment': 'W1D3-5',
+    'funRated': '4',
+    'dificulty': '4'
+  },
+  {
+    'Name': 'Evelyn',
+    'asignment': 'W1D3 - Project - Guess-the-number',
+    'funRated': '5',
+    'dificulty': '5'
+  },
+  {
+    'Name': 'Evelyn',
+    'asignment': 'W1D4-1',
+    'funRated': '5',
+    'dificulty': '4'
+  },
+  {
+    'Name': 'Evelyn',
+    'asignment': 'W1D4 - Project - Kleurentoggle',
+    'funRated': '4',
+    'dificulty': '3'
+  },
+  {
+    'Name': 'Evelyn',
+    'asignment': 'W1D5 - Project - Galgje',
+    'funRated': '3',
+    'dificulty': '3'
+  },
+  {
+    'Name': 'Evelyn',
+    'asignment': 'W2D1-1',
+    'funRated': '5',
+    'dificulty': '1'
+  },
+  {
+    'Name': 'Evelyn',
+    'asignment': 'W2D1-2',
+    'funRated': '5',
+    'dificulty': '2'
+  },
+  {
+    'Name': 'Evelyn',
+    'asignment': 'W2D2-1',
+    'funRated': '4',
+    'dificulty': '3'
+  },
+  {
+    'Name': 'Evelyn',
+    'asignment': 'W2D2-2',
+    'funRated': '4',
+    'dificulty': '3'
+  },
+  {
+    'Name': 'Evelyn',
+    'asignment': 'W2D2-3',
+    'funRated': '4',
+    'dificulty': '3'
+  },
+  {
+    'Name': 'Evelyn',
+    'asignment': 'W2D3-1',
+    'funRated': '4',
+    'dificulty': '4'
+  },
+  {
+    'Name': 'Evelyn',
+    'asignment': 'W2D3-2',
+    'funRated': '3',
+    'dificulty': '1'
+  },
+  {
+    'Name': 'Evelyn',
+    'asignment': 'W2D3-3',
+    'funRated': '4',
+    'dificulty': '4'
+  },
+  {
+    'Name': 'Evelyn',
+    'asignment': 'W2D4-1',
+    'funRated': '3',
+    'dificulty': '1'
+  },
+  {
+    'Name': 'Evelyn',
+    'asignment': 'W2D4-2',
+    'funRated': '4',
+    'dificulty': '4'
+  },
+  {
+    'Name': 'Evelyn',
+    'asignment': 'W2D4-3',
+    'funRated': '3',
+    'dificulty': '1'
+  },
+  {
+    'Name': 'Evelyn',
+    'asignment': 'W2D5 - Project - Filmzoeker',
+    'funRated': '4',
+    'dificulty': '3'
+  },
+  {
+    'Name': 'Evelyn',
+    'asignment': 'W3D1-1',
+    'funRated': '4',
+    'dificulty': '3'
+  },
+  {
+    'Name': 'Evelyn',
+    'asignment': 'W3D1-2',
+    'funRated': '4',
+    'dificulty': '2'
+  },
+  {
+    'Name': 'Evelyn',
+    'asignment': 'W3D1-3',
+    'funRated': '4',
+    'dificulty': '3'
+  },
+  {
+    'Name': 'Evelyn',
+    'asignment': 'W3D1-4',
+    'funRated': '3',
+    'dificulty': '3'
+  },
+  {
+    'Name': 'Evelyn',
+    'asignment': 'W3D2-1',
+    'funRated': '4',
+    'dificulty': '2'
+  },
+  {
+    'Name': 'Evelyn',
+    'asignment': 'W3D2-2',
+    'funRated': '3',
+    'dificulty': '3'
+  },
+  {
+    'Name': 'Evelyn',
+    'asignment': 'W3D2-3',
+    'funRated': '5',
+    'dificulty': '3'
+  },
+  {
+    'Name': 'Evelyn',
+    'asignment': 'W3D3-1',
+    'funRated': '3',
+    'dificulty': '3'
+  },
+  {
+    'Name': 'Evelyn',
+    'asignment': 'W3D3-2',
+    'funRated': '5',
+    'dificulty': '4'
+  },
+  {
+    'Name': 'Evelyn',
+    'asignment': 'W3D3-3',
+    'funRated': '5',
+    'dificulty': '3'
+  },
+  {
+    'Name': 'Evelyn',
+    'asignment': 'W3D3-4',
+    'funRated': '5',
+    'dificulty': '4'
+  },
+  {
+    'Name': 'Evelyn',
+    'asignment': 'W3D4-1',
+    'funRated': '5',
+    'dificulty': '5'
+  },
+  {
+    'Name': 'Evelyn',
+    'asignment': 'W3D4-2',
+    'funRated': '5',
+    'dificulty': '1'
+  },
+  {
+    'Name': 'Evelyn',
+    'asignment': 'W3D5 - Project - Todo-List',
+    'funRated': '5',
+    'dificulty': '2'
+  },
+  {
+    'Name': 'Evelyn',
+    'asignment': 'W4D2-1',
+    'funRated': '5',
+    'dificulty': '1'
+  },
+  {
+    'Name': 'Evelyn',
+    'asignment': 'W4D2-2',
+    'funRated': '5',
+    'dificulty': '1'
+  },
+  {
+    'Name': 'Evelyn',
+    'asignment': 'W4D2-3',
+    'funRated': '5',
+    'dificulty': '1'
+  },
+  {
+    'Name': 'Evelyn',
+    'asignment': 'W4D2-4',
+    'funRated': '5',
+    'dificulty': '1'
+  },
+  {
+    'Name': 'Evelyn',
+    'asignment': 'W4D3-1',
+    'funRated': '5',
+    'dificulty': '1'
+  },
+  {
+    'Name': 'Evelyn',
+    'asignment': 'W4D3-2',
+    'funRated': '5',
+    'dificulty': '1'
+  },
+  {
+    'Name': 'Evelyn',
+    'asignment': 'W4D3-3',
+    'funRated': '5',
+    'dificulty': '1'
+  },
+  {
+    'Name': 'Evelyn',
+    'asignment': 'W4D3-4',
+    'funRated': '5',
+    'dificulty': '1'
+  },
+  {
+    'Name': 'Evelyn',
+    'asignment': 'W4D3-5',
+    'funRated': '5',
+    'dificulty': '1'
+  },
+  {
+    'Name': 'Evelyn',
+    'asignment': 'W4D3 - Project - Next-Level CSS',
+    'funRated': '5',
+    'dificulty': '1'
+  },
+  {
+    'Name': 'Evelyn',
+    'asignment': 'W5D4-1',
+    'funRated': '4',
+    'dificulty': '3'
+  },
+  {
+    'Name': 'Evelyn',
+    'asignment': 'W5D5 - Project - Lil_Playlist',
+    'funRated': '3',
+    'dificulty': '4'
+  },
+  {
+    'Name': 'Evelyn',
+    'asignment': 'W6D1-1',
+    'funRated': '5',
+    'dificulty': '5'
+  },
+  {
+    'Name': 'Evelyn',
+    'asignment': 'W6D2-1',
+    'funRated': '3',
+    'dificulty': '4'
+  },
+  {
+    'Name': 'Evelyn',
+    'asignment': 'W6D2 - Project - Eindopdracht',
+    'funRated': '5',
+    'dificulty': ''
+  },
+  {
+    'Name': 'Evelyn',
+    'asignment': '',
+    'funRated': '',
+    'dificulty': ''
+  },
 ];
 
 export default StudentData;
